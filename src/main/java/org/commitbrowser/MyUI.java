80ec50f19c521979a9bd4f02fea42f284338df92
Markus Koivisto <markus@vaadin.com>
2015-05-11 12:11:06 +0300

    Columns and Detailsrow now match spec.
    Cleaned up UI. Added progress bar for size. Added missing details
    generator values.

--- src/main/java/org/commitbrowser/MyUI.java
+++ src/main/java/org/commitbrowser/MyUI.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 
 
 
+
+
+
+
 //import com.vaadin.ui.Grid.DetailsGenerator;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
@@ -17,16 +21,20 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.Column;
 import com.vaadin.ui.Grid.DetailsGenerator;
 import com.vaadin.ui.Grid.RowReference;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.renderers.ProgressBarRenderer;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.event.ItemClickEvent;
@@ -46,15 +54,13 @@ public class MyUI extends UI {
     @Inject
     GitRepositoryService gitRepositoryService;
     private TextField numberTextField;
-    private TextArea fullMessage;
     private Grid grid = new Grid();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
     	
     	Layout layout = new VerticalLayout();
-    	fullMessage = new TextArea();
-    	fullMessage.setWidth("1400px");
+
         
 
         LazyList<Commit> lazyList = new LazyList<>(
@@ -67,67 +73,36 @@ public class MyUI extends UI {
         for(Column c : columns){
         	System.out.println("c.getPropertyId() = "+c.getPropertyId());
         }
-        
-        //remove size
-        grid.removeColumn(columns.get(1).getPropertyId());
-        //remove full message
-        grid.removeColumn(columns.get(1).getPropertyId());
-        
-        grid.setSizeFull();
-        //grid.setWidth("1400px");
 
-        layout.addComponent(grid);
-        layout.addComponent(fullMessage);
+          
+        //render size as progressbar
+        columns.get(1).setRenderer(new ProgressBarRenderer());
         
+     
+        //remove commit time
+        grid.removeColumn(columns.get(2).getPropertyId());
+        
+        //remove full message
+        grid.removeColumn(columns.get(3).getPropertyId());
+        
+        layout.setSizeFull();
+        grid.setSizeFull();
+        
+        layout.addComponent(grid);
         
         grid.addItemClickListener(new ItemClickEvent.ItemClickListener(){
 
 			@Override
 			public void itemClick(ItemClickEvent event) {
-				Commit commit = (Commit)event.getItemId();
-				fullMessage.setValue(commit.getFullMessage());
+				 Object itemId = event.getItemId();
+			     boolean isVisible = grid.isDetailsVisible(itemId);
+			     grid.setDetailsVisible(itemId, !isVisible);
 			}
         	
         }
         );
+        grid.setDetailsGenerator(detailsGenerator);
         
-        
-        
-        
-        final CheckBox checkbox = new CheckBox("Details generator");
-        checkbox.addValueChangeListener(new ValueChangeListener() {
-            @Override
-            @SuppressWarnings("boxing")
-            public void valueChange(ValueChangeEvent event) {
-                if (checkbox.getValue()) {
-                    grid.setDetailsGenerator(detailsGenerator);
-                } else {
-                    grid.setDetailsGenerator(DetailsGenerator.NULL);
-                }
-            }
-        });
-        layout.addComponent(checkbox);
-        
-        numberTextField = new TextField("Row");
-        numberTextField.setImmediate(true);
-        layout.addComponent(numberTextField);
-
-        layout.addComponent(new Button("Toggle and scroll",
-                new Button.ClickListener() {
-                    @Override
-                    public void buttonClick(ClickEvent event) {
-                        toggle();
-                        scrollTo();
-                    }
-                }));
-        layout.addComponent(new Button("Scroll and toggle",
-                new Button.ClickListener() {
-                    @Override
-                    public void buttonClick(ClickEvent event) {
-                        scrollTo();
-                        toggle();
-                    }
-                }));
         
         setContent(layout);
 
@@ -136,14 +111,24 @@ public class MyUI extends UI {
     private final DetailsGenerator detailsGenerator = new DetailsGenerator() {
         @Override
         public Component getDetails(RowReference rowReference) {
+        	FormLayout layout = new FormLayout();
+        	        	
             Commit commit = (Commit) rowReference.getItemId();
-            TextArea textArea = new TextArea();
+            
+            DateField commitDate = new DateField("Commit Timestamp");
+            commitDate.setValue(commit.getCommitTime());
+            layout.addComponent(commitDate);
+            
+            DateField authorDate = new DateField("Author Timestamp");
+            authorDate.setValue(commit.getTimestamp());
+            layout.addComponent(authorDate);
+            
+            TextArea textArea = new TextArea("Commit Message");
             textArea.setValue(commit.getFullMessage());
-            return textArea;
-            // currently the decorator row doesn't change its height when the
-            // content height is different.
-           // label.setHeight("30px");
-            //return label;
+            textArea.setWidth("100%");
+            layout.addComponent(textArea);
+            
+            return layout;
         }
     };
     
